1feb64e896018471f65745f582f32c98b2f4c519
Rajeev <rajeevkumarsharma@outlook.com>
2019-07-17 10:46:36 +0530

    local data

--- src/components/home/articles/articleList.js
+++ src/components/home/articles/articleList.js
@@ -1,14 +1,15 @@
 const LATEST_ARTICLES = [
   {
-    HEADLINE: "Life is 10% what happens to you and 90% how you react to it. ",
-    MESSAGE: "",
+    HEADLINE: "Life is 10%... ",
+    MESSAGE: "Life is 10% what happens to you and 90% how you react to it. ",
     BY: "Charles R. Swindoll",
     DATE: ""
   },
   {
-    HEADLINE: "Life is 10% what happens to you and 90% how you react to it.",
-    MESSAGE: "",
-    BY: "Charles R. Swindoll",
+    HEADLINE: "Good, better, best...",
+    MESSAGE:
+      "Good, better, best. Never let it rest. 'Til your good is better and your better is best",
+    BY: "St. Jerome",
     DATE: ""
   }
 ];
